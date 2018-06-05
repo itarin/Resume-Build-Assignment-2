f208c023d5a68ad187a2899c5d83ee4e57254ae5
Ivan Tarin <ivan0tarin@techtonicgroup.com>
2018-06-05 08:34:55 -0600

    added rock paper scissors challenge to python file

---
+++ python/rock_paper_scissors.py
@@ -0,0 +1,26 @@
+print("Rock...")
+print("Paper...")
+print("Scissors...")
+player1 = input(" Make your move : ")
+print("*** NO CHEATING!!!***\n\n" * 20)
+player2 = input(" Make your move ")
+
+if player1 == player2:
+    print("It's a tie!")
+elif player1 == "rock":
+    if player2 == "scissors":
+        print("player1 wins!")
+    elif player2 == "paper":
+        print("player2 wins!")
+elif player1 == "paper":
+    if player2 == "rock":
+        print("player1 wins!")
+    elif player2 == "scissors":
+        print("player2 wins!")
+elif player1 == "scissors":
+    if player2 == "paper":
+        print("player1 wins!")
+    elif player2 == "rock":
+        print("player2 wins!")
+else:
+    print("Something went wrong")
