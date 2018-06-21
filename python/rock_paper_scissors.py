d89989216996fe630713e9af4bbf0b063a38b0bf
Ivan Tarin <ivan0tarin@techtonicgroup.com>
2018-06-20 19:35:51 -0600

    adding a couple of python projects

--- python/rock_paper_scissors.py
+++ python/rock_paper_scissors.py
@@ -1,26 +1,37 @@
 print("Rock...")
 print("Paper...")
 print("Scissors...")
-player1 = input(" Make your move : ")
-print("*** NO CHEATING!!!***\n\n" * 20)
-player2 = input(" Make your move ")
+player1 = input(" Make your move : ").lower()
+# print("*** NO CHEATING!!!***\n\n" * 20)
+import random import randint
 
-if player1 == player2:
+random_num = randint(1,3)
+
+if random_num == 1:
+    computer = "paper"
+elif random_num == 2:
+    computer = "rock"
+else:
+    computer = "scissors"
+# text  = f"Computer plays {computer}"
+print(f"Computer plays: {computer}")
+
+if player1 == computer:
     print("It's a tie!")
 elif player1 == "rock":
-    if player2 == "scissors":
+    if computer == "scissors":
         print("player1 wins!")
-    elif player2 == "paper":
-        print("player2 wins!")
+    else:
+        print("computer wins!")
 elif player1 == "paper":
-    if player2 == "rock":
+    if computer == "rock":
         print("player1 wins!")
-    elif player2 == "scissors":
-        print("player2 wins!")
+    else:
+        print("computer wins!")
 elif player1 == "scissors":
-    if player2 == "paper":
+    if computer == "paper":
         print("player1 wins!")
-    elif player2 == "rock":
-        print("player2 wins!")
+    else:
+        print("computer wins!")
 else:
-    print("Something went wrong")
+    print("Please, enter a valid move")
