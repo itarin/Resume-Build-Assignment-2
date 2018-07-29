97cf84bc20c27f869258c319e42fc5d6bf655400
ivan tarin <itarin>
2018-07-29 00:00:56 -0600

    skeleton website

--- expess_app/express-locallibrary-tutorial/routes/index.js
+++ expess_app/express-locallibrary-tutorial/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'TITLE' });
+}).get('/users/cool', function(req, res, next) {
+  res.render('index', { title: 'ROSY TE AMO' });
 });
 
 module.exports = router;
