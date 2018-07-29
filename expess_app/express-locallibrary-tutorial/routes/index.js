784f65137e4dae8e134ffd038d4397403da6eccf
ivan tarin <itarin>
2018-07-29 15:01:21 -0600

    starting to integrate database

--- expess_app/express-locallibrary-tutorial/routes/index.js
+++ expess_app/express-locallibrary-tutorial/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'TITLE' });
 }).get('/users/cool', function(req, res, next) {
-  res.render('index', { title: 'ROSY TE AMO' });
+  res.render('index', { title: 'Jose Q' });
 });
 
 module.exports = router;
