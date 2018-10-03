b573c292c9062bc3c228d9e36e8e0f2dad4e0e3f
ivan tarin <itarin>
2018-10-03 08:22:58 -0600

    parsed to get the info on the data dump sizes and contents, deciding whether spending 2 hrs to download all of wikipedia is a good idea?

--- wikipedia_scraper/wikipedia_scraper.py
+++ wikipedia_scraper/wikipedia_scraper.py
@@ -3,20 +3,24 @@ import requests
 #Library for parsing HTML
 from bs4 import BeautifulSoup
 
-base_url = 'htps://dumps.wikimedia.org/enwiki/'
+base_url = 'https://dumps.wikimedia.org/enwiki/'
 index = requests.get(base_url).text
 soup_index = BeautifulSoup(index, 'html.parser')
 
 # Find the links on the page
 dumps = [a['href'] for a in soup_index.find_all('a') if a.has_attr('href')]
-dumps
 
-['../',
- '20180620/',
- '20180701/',
- '20180720/',
- '20180801/',
- '20180820/',
- '20180901/',
- '20180920/',
- 'latest/']
+# print(dumps)this was used to print the dumps
+
+# For this project, we’ll take the dump on September 1, 2018
+# To find all the available files in the dump, we use the following code:
+dump_url = base_url + '20180901/'
+
+# Retrieve th HTML
+dump_html = requests.get(dump_url).text
+
+# Convert to a soup
+soup_dump = BeautifulSoup(dump_html, 'html.parser')
+
+# Find list elements with the class file, this can also be printed to check li's
+soup_dump.find_all('li', {'class': 'file'})[:3]
