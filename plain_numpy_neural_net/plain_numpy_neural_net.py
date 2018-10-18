b95c082c2d703c1d9f7f44e359de00a9b6cf67b6
ivan tarin <itarin>
2018-10-18 15:47:12 -0600

    finished the NN exercise, but dont have data to test atm

--- plain_numpy_neural_net/plain_numpy_neural_net.py
+++ plain_numpy_neural_net/plain_numpy_neural_net.py
@@ -74,9 +74,91 @@ def full_forward_propagation(X, params_values, nn_architecture):
 
         activ_function_curr = layer["activation"]
         W_curr = params_values["W" + str(layer_idx)]
-        b_crr = params_values["b" + str(layer_idx)]
+        b_curr = params_values["b" + str(layer_idx)]
         A_curr, Z_curr = single_layer_forward_propagation(A_prev, W_curr, b_curr, activ_function_curr)
         memory["A" + str(idx)] = A_prev
         memeory["Z" + str(layer_idx)] = Z_curr
 
     return A_curr, memory
+
+"""
+The Loss Function:
+Generally speaking, the loss function is designed to show how far we are from the ‘ideal’ solution. It is selected according to the problem to be solved.
+We are using a bianary function because we are chosing between 2 datasets. The bianary function is called 'binary crossentropy'.
+"""
+def get_cost_value(Y_hat, Y):
+    m = Y_hat.shape[1]
+    cost = -1/m * ( np.dot(Y, np.log(Y_hat).T) + np.dot(1-Y, np.log(1-Y_Hat).T) )
+    return np.squeeze(cost)
+
+def get_accuracy_value(Y_hat, Y):
+    Y_hat_ = convert_prob_into_class(Y_hat)
+    return (Y_hat_ == Y).all(axis=0).mean()
+
+"""
+Backward Propagation - is often intimidating to people as it uses differential calculus and linear algebra
+Remember backward propagation != gradient descent
+"""
+def single_layer_backward_propagation(dA_curr, W_curr, b_curr, Z_curr, A_prev, activation="relu"):
+    m = A_prev.shape[1]
+
+    if activation is "relu":
+        backward_activation_func = relu_backward
+    elif activation is "sigmoid":
+        backward_activation_func = sigmoid_backward
+    else:
+        raise Exception('Non-supported activation runction')
+
+    dZ_curr = backward_activation_func(dA_curr, Z_curr)
+    dW_curr = np.dot(dZ_curr, A_prev.T)/m
+    db_curr = np.sum(dZ_curr, axis=1, keepdims=True)/m
+    dA_prev = np.dot(W_curr.T, dZ_curr)
+
+    return dA_prev, dW_curr, db_curr
+
+def full_backward_propagation(Y_hat, Y, memory, params_values, nn_architecture):
+    grads_values = {}
+    m = Y.shape[1]
+    Y = Y.reshape(Y_hat.shape)
+
+    dA_prev = - (np.divide(Y, Y_hat) - np.divide(1 - Y, 1 - Y_hat));
+
+    for layer _idx_prev, layer in reversed(list(enumerate(nn_architecture))):
+        layer_idx_curr = layer_idx_prev + 1
+        activ_function_curr = layer["activation"]
+
+        dA_curr = dA_prev
+
+        A_prev = memory["A" + str(layer_idx_prev)]
+        Z_curr = memory["Z" + str(layer_idx_curr)]
+        W_curr = params_values["W" + str(layer_idx_curr)]
+        b_curr = params_values["b" + str(layer_idx_curr)]
+
+        dA_prev, dW_curr, db_curr, = single_layer_backward_propagation( dA_curr, W_curr, b_curr, Z_curr, A_prev, activ_function_curr)
+
+        grads_values["dW" + str(layer_idx_curr)] = dW_curr
+        grads_values["db" + str(layer_idx_curr)] = db_curr
+
+    return grads_values
+
+# Updating parameter values using gradient optimization, this is to bring the target function closer to a minimum
+# To do this we use two dictionaries the params_values and grads_values as function parameters
+def update(params_values, grads_values, nn_architecture, learning_rate):
+    for idx, layer in enumerate(nn_architecture):
+        layer_idx = idx + 1
+        params_values["W" + str(layer_idx)] -= learning_rate * grads_values["dW" + str(layer_idx)]
+        params_values["b" + str(layer_idx)] -= learning_rate * grads_values["db" + str(layer_idx)]
+
+    return params_values;
+
+def train(X, Y, nn_architecture, epochs, learning_rate):
+    params_values = init_layers(nn_architecture, 2)
+    cost_history = []
+    accuracy_history = []
+
+    for i in range(epochs):
+        Y_hat, cashe = full_forward_propagation( X, params_values, nn_architecture)
+        cost = get_cost_value(Y_hat, Y)
+        cost_history.append(cost)
+        accuracy = get_accuracy_value(Y_hat, Y)
+        accuracy_history.append(accuracy)
